4d7af815552dd020fbcc04247466621692b702ae
Chris Mather <mather.chris@gmail.com>
2013-05-08 15:59:41 -0700

    removed meteor-file package

--- packages/meteor-file/package.js
+++
@@ -1,8 +0,0 @@
-Package.describe({
-  summary: "Simple file uploading for Meteor"
-});
-
-Package.on_use(function (api) {
-  api.use(["ejson"], ["client", "server"]);
-  api.add_files(["meteor-file.js"], ["client", "server"]);
-});
